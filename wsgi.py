a9c99f0d92b000dccc5d0de76825a3dd1dae40c9
Salman Rahman <salman.rahman@se14.qmul.ac.uk>
2018-06-23 00:15:50 +0100

    deploy api: after local test ok

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python
 
 from flask import Flask
+from flask_restful import Resource, Api
+from OpDefine import DFX
 
 __authon__ = "salmansamie"
 
 
 application = Flask(__name__)
+fsk_api = Api(application)
 
 
-@application.route("/")
-def app_root():
-    return "test deploy"
+class OpenDefineApi(Resource):
+    def get(self):
+        return {
+            "project author": "Salman Rahman (salmansamie)",
+            "source code": "https://github.com/salmansamie/OpDefine.git",
+            "license": "GNU GPL v3.0"
+        }
+
+
+class GetMultipart(Resource):
+    def get(self, param):
+        return DFX(param).define()
 
 
 if __name__ == "__main__":
+    fsk_api.add_resource(OpenDefineApi, '/')
+    fsk_api.add_resource(GetMultipart, '/api/v3/<string:param>')
     application.run()
